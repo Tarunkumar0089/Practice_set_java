8a23d84a47b7eca2e3773d7545c0d3abf25a83d0
Tarunkumar0089 <tarunkumar321028@gmail.com>
2025-03-21 21:25:56 +0530

    new

--- first_unique_char.java
+++ first_unique_char.java
@@ -8,19 +8,16 @@ public class first_unique_char {
 
     public static int firstUniqChar(String s) {
         HashMap<Character, Integer> charCount = new HashMap<>();
-
-        // Count frequency of each character
         for (char c : s.toCharArray()) {
             charCount.put(c, charCount.getOrDefault(c, 0) + 1);
         }
 
-        // Find first character with frequency 1
         for (int i = 0; i < s.length(); i++) {
             if (charCount.get(s.charAt(i)) == 1) {
                 return i;
             }
         }
 
-        return -1; // Return -1 if no unique character found
+        return -1; 
     }
 }
---
+++ reverse_word.java
@@ -0,0 +1,9 @@
+public class reverse_word {
+    public static void main(String[] args) {
+        String str="the sky is blue";
+        String[] ch=str.split(" ");
+        for(int i=ch.length-1;i>=0;i--){
+             System.out.print(ch[i]+" ");
+        }
+    }
+}
